6faae2afad9e9a194a0af466111529c9ebc99849
fcventura02 <filipecvr4@gmail.com>
2026-03-30 22:08:39 -0300

    feat: install chalk dependency to enhance console output styling

---
+++ 2_FUNDAMENTOS/08_melhorando_visual/index.mjs
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+const nota = 8;
+
+if (nota >= 7) {
+    console.log(chalk.green.bold("Parabéns, você passou!"));
+} else {
+    console.log(chalk.bgRed.black("Você precisa fazer a prova final!"));
+}
