09fb2c9944e8fa50940c043d05e71f957064f083
fcventura02 <filipecvr4@gmail.com>
2026-03-30 23:04:44 -0300

    chore: Create application accepted external packages inquirer and chalk

---
+++ 2_FUNDAMENTOS/15_tarefa_02/index.mjs
@@ -0,0 +1,17 @@
+import inquirer from 'inquirer'
+import chalk from 'chalk';
+
+
+const prompt = inquirer.createPromptModule();
+
+prompt([
+    { name: 'nome', message: 'Qual o seu nome?' },
+    { name: 'idade', message: 'Qual a sua idade?' },
+])
+    .then((answers) => {
+        const response = `O nome do usuário é ${answers.nome} e ele tem ${answers.idade} anos`
+        console.log(chalk.bgYellow.black(response));
+    })
+    .catch((err) => {
+        console.log(err)
+    })
