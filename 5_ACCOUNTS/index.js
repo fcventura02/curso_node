8e17887eace087a3590d90ec5597a8da0557aaab
fcventura02 <filipecvr4@gmail.com>
2026-04-02 18:30:19 -0300

    feat: implement account creation logic using inquirer and fs modules

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -6,9 +6,10 @@ import fs from "fs";
 const prompt = inquirer.createPromptModule();
 
 function operation() {
+    console.clear();
     prompt([
         {
-            type: 'list',
+            type: 'rawlist',
             name: 'action',
             message: 'O que deseja fazer?',
             choices: [
@@ -22,12 +23,49 @@ function operation() {
     ]).then((answer) => {
         const action = answer['action'];
         if (action === 'Criar conta') {
-            console.log(chalk.bgGreen.black('Conta criada com sucesso!'));
+            createAccount();
         }
     }).catch((error) => {
         console.log(error);
     });
 
+}
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Conta criada com sucesso!'));
+    console.log(chalk.green('Defina as opções da sua conta:'));
+    buildAccount();
+}
+
+function buildAccount() {
+    prompt([
+        {
+            type: 'input',
+            name: 'accountName',
+            message: 'Digite o nome da sua conta:'
+        }
+    ]).then((answer) => {
+        const accountName = answer['accountName'];
+        console.info(accountName);
+
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts');
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Conta já existe!'));
+            buildAccount();
+            return;
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function (err) {
+            console.log(err);
+        });
+        console.log(chalk.bgGreen.black('Conta criada com sucesso!'));
+        operation();
+    }).catch((error) => {
+        console.log(error);
+    });
 }
 
 operation();
