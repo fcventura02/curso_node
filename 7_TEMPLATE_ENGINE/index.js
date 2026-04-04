5dfa634b54319dec887ed13353c49d499444a189
fcventura02 <filipecvr4@gmail.com>
2026-04-04 12:23:11 -0300

    feat: implement template and data presentation

--- 7_TEMPLATE_ENGINE/index.js
+++ 7_TEMPLATE_ENGINE/index.js
@@ -7,9 +7,16 @@ app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-    res.render("home", { layout: false });
+    const user = {
+        name: "Filipe",
+        age: 22,
+    };
+
+    const job = "Programador";
+    res.render("home", { user, job });
 });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
+    console.log("http://localhost:3000");
 });
